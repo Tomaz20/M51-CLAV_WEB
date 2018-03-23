56966c1a04ff725b509ec7d0b3e874ef1800373b
Tomás Ferreira <ttomaz.200@gmail.com>
2018-03-23 14:05:00 +0000

    merge

---
+++ public/javascripts/users/pedido_submetido.vue
@@ -0,0 +1,36 @@
+var orgs = new Vue({
+    el: '#pedido',
+    data: {
+        titulo: "Pedido Submetido",
+        cabecalho: ["Nº","Tipo","Descrição","Data de Submissão",""],
+        linhas: [
+            [
+                "1/032018",
+                "Teste",
+                "Texto Exemplo",
+                "23/03/2018",
+                "<div class='button-darker'><a href='#'>Ver Pedido</a></div>"
+            ]
+        ]
+    },
+    methods: {
+        verpedido: function(id){
+            console.log(id);
+        }
+    },
+    created: function(){
+        /*
+        this.$http.get("")
+        .then( function(response) { 
+            this.content = response.body;
+        })
+        .then( function() {
+            this.parse();
+            this.ready=true;
+        })
+        .catch( function(error) { 
+            console.error(error); 
+        });
+        */
+    }
+})
